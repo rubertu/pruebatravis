af297d590e37b79bdeed8e55ea0bf015c042a9c8
Roberto Zamora Jimenez <rubertu@gmail.com>
2015-09-22 16:38:30 +0200

    no message

---
+++ TravisTest/Class1.cs
@@ -0,0 +1,19 @@
+﻿
+using NUnit.Framework;
+
+namespace TravisTest
+{
+    [TestFixture]
+    public class Class1
+    {
+
+        [Test]
+        public static void Test1()
+        {
+            int a = 1;
+            int b = 1;
+
+            Assert.AreEqual(a,b);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using TravisTest;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void MyTest1()
+        {
+            // arrange
+
+            Assert.AreEqual(Class1.Test1(1,1), 2);
+        }
+    }
+}
